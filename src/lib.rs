815a5573661213b6149528779267e94eba26fbb5
Markus Unterwaditzer <markus@unterwaditzer.net>
2016-03-25 00:03:11 +0100

    Clippy leftovers

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,5 @@
 // DOCS
 
-#![cfg_attr(feature = "clippy", allow(unstable_features))]
-#![cfg_attr(feature = "clippy", feature(plugin))]
-#![cfg_attr(feature = "clippy", plugin(clippy))]
-#![cfg_attr(feature = "clippy", deny(warnings))]
-
 extern crate iron;
 extern crate oven;
 extern crate cookie;
